6a35abdea147b697bd3972022f11ea73c1710605
Drita Guta <drita.guta@dci-student.org>
2024-05-08 16:23:52 +0200

    fetch Movie Details

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import fetchMovieDetails from "../components/fetch/FetchDetails";
+
 const Homepage = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const movieDetails = await fetchMovieDetails();
+        console.log("Movie Details:", movieDetails);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return <div>Homepage</div>;
 };
 
