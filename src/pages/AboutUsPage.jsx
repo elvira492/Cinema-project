bfeee6750efa7afff7bbba9de3c7f55e4bf185c1
elvira492 <elvira.dederer.ed@gmail.com>
2024-05-10 11:58:34 +0200

    about us page, map added

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,51 +1,70 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 const AboutUsPage = () => {
+  const centerCoordinates = [52.526597, 13.374156]; // Koordinaten
+
   return (
-    <div>
-      AboutUsPage Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      Commodi illo tempora libero, ab quibusdam veniam molestias ipsa vel
-      perferendis esse sint autem exercitationem qui animi at, dicta
-      reprehenderit ea nisi earum accusamus quaerat incidunt cupiditate? Quasi
-      vitae consequatur maxime mollitia non, aspernatur quam beatae culpa,
-      reprehenderit repudiandae inventore suscipit accusamus aliquam fugiat?
-      Commodi aut officiis dicta fugiat necessitatibus modi veritatis nulla rem
-      nobis sit earum, sunt cum expedita dignissimos facere! Veritatis, nobis
-      veniam. Nulla, natus veritatis in accusantium exercitationem fuga id vero
-      placeat eius laudantium unde numquam quibusdam voluptatem? Repudiandae,
-      voluptatem totam? Omnis hic cupiditate qui eos. Ad officia sed asperiores,
-      consectetur voluptates mollitia porro officiis itaque autem vero ipsum est
-      accusamus suscipit odit beatae provident iusto aliquam saepe quibusdam
-      deserunt ab tempora laboriosam. Voluptatum voluptatem rerum est ex
-      praesentium ducimus, illum corporis veritatis itaque repellendus commodi
-      omnis. Architecto suscipit nobis odit praesentium consectetur aut
-      deserunt, libero quia officiis iusto nulla, facilis aspernatur voluptas.
-      Quod soluta explicabo ab non animi? Tempore dicta soluta eum, nemo
-      repudiandae facilis explicabo non veritatis tempora voluptates magnam
-      officia. Repudiandae obcaecati, provident ipsa ex aut, a modi natus quidem
-      nisi incidunt iste! Numquam dicta veritatis obcaecati, unde magni quasi
-      eius maiores. Repudiandae tempore iure voluptatum saepe alias, dolorum,
-      reprehenderit optio officia totam adipisci modi minus qui iusto. Fugit
-      quibusdam ab veritatis, accusantium voluptatibus, harum blanditiis
-      perferendis iste autem libero veniam cumque! Harum excepturi hic
-      exercitationem atque earum dicta corrupti dolore quasi, illum quibusdam
-      accusantium vel quod consectetur nam quos vitae reiciendis nesciunt
-      sapiente placeat! Laboriosam libero eum, aut unde animi error aspernatur
-      ab atque, eius incidunt ad at molestias quas culpa nemo. Eos, eaque quia,
-      odio vel dolores magnam in eum animi amet consectetur laborum! Quae quia
-      provident ullam adipisci qui omnis iure dignissimos, sequi officiis.
-      Excepturi labore illum doloribus dolore consectetur. Voluptates at
-      praesentium dignissimos recusandae nam beatae ea enim, voluptate
-      accusantium quibusdam laboriosam officia doloremque pariatur voluptatum ex
-      qui. Incidunt nesciunt quibusdam, dolorum qui sunt iusto tempore, suscipit
-      officiis eveniet iste expedita recusandae placeat molestias minus commodi
-      nobis veniam ut, non sequi repellendus. Explicabo incidunt rem temporibus
-      velit iure impedit saepe, quasi ipsam provident voluptates magnam iste
-      dolorum aperiam ex praesentium aut itaque sed quia blanditiis sunt nam, a
-      repellat? Quis labore consequatur sequi nulla tenetur sed vitae. Pariatur
-      odio saepe sit illo, voluptatem eius, culpa tempore perspiciatis facere
-      doloribus nostrum nisi commodi quidem eos vero modi quos error ratione
-      aliquam, laborum molestiae animi dicta rerum dolorem? Similique
+    <div className="flex flex-col md:flex-row justify-center items-center p-8 md:p-16">
+      {/* Textinhalt */}
+      <div className="flex-1 max-w-lg">
+        <h3 className="text-xl font-semibold mb-4 text-center">About Us</h3>
+        <div className="mb-4">
+          <p className="font-semibold">Opening Hours</p>
+          <p>
+            Fantasy Cinema is open: Monday – Friday from 11:00 PM onwards
+            Saturday – Sunday 15 minutes before the start of the first
+            screening. You can find a program overview{" "}
+            <a href="/program">here</a>.
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="font-semibold">Directions to Fantasy Cinema</p>
+          <p>
+            Fantasy Cinema is located directly at the intersection of
+            Hans-Sachs- and Ickstattstraße in the Glockenbachviertel district
+            and is easily accessible on foot from the Sendlinger Tor or
+            Fraunhoferstraße train stations.
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="font-semibold">Public Transportation</p>
+          <p>
+            U1/2: Fraunhoferstraße Station, U3/6: Sendlinger Tor Station Tram
+            16/17/18: Müllerstraße Stop By Car Public parking spaces around the
+            cinema are very limited. Please consider walking, cycling, or using
+            public transportation (MVG).
+          </p>
+        </div>
+      </div>
+
+      {/* Karte */}
+      <div className="flex-1 max-w-lg">
+        <MapContainer
+          center={centerCoordinates}
+          zoom={15}
+          style={{ height: "400px", width: "100%" }}
+          className="rounded-md shadow-lg"
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          />
+          <Marker position={centerCoordinates}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
   );
 };
 
 export default AboutUsPage;
+
+/* 
+OpenStreetMap (OSM) 
+npm install leaflet react-leaflet
+Koordinaten: 52° 31' 35,75'' N, 13° 22' 26,96'' E  -52.526597, 13.374156
+ */
